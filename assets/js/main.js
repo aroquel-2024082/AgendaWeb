9a989f686203847db3ce5167ca17e6ad6fc41bd5
aroquel-2024082 <aroquel-2024082@kinal.edu.gt>
2026-01-15 17:50:01 -0600

    Se hizo responsive el login ya que no lo era aun

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    
-    const btnLogin = document.getElementById('btnLogin');
-    const emailInput = document.getElementById('emailInput');
-
-    if (btnLogin && emailInput) {
-        btnLogin.addEventListener('click', () => {
-            const emailValue = emailInput.value.trim();
-
-            if (emailValue === "") {
-                alert("Por favor, ingresa tu correo electrónico.");
-            } else {
-                localStorage.setItem('usuarioAgenda', emailValue);
-                
-                window.location.href = "pages/principal.html"; 
-            }
-        });
-    }
-
-    const displayNombre = document.getElementById('nombreUsuario');
-
-    if (displayNombre) {
-        const usuarioGuardado = localStorage.getItem('usuarioAgenda');
-
-        if (usuarioGuardado) {
-            const nombreLimpio = usuarioGuardado.split('@')[0];
-            displayNombre.innerText = "Bienvenido " + nombreLimpio;
-        } else {
-            window.location.href = "../index.html";
-        }
-    }
-});
-
 // Modal de Favoritos
 // si se ponen en el mismo que el otro no funciona el de contactos
 
